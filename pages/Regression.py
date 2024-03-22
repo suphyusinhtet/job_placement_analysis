c4148c5bd867637ebf52635012929e2053a29779
suphyusinhtet <53178539+suphyusinhtet@users.noreply.github.com>
2024-03-22 22:27:13 +0630

    Update Regression.py

--- pages/Regression.py
+++ pages/Regression.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 import pickle
 from sklearn.model_selection import train_test_split
 rfmodel= st.sidebar.checkbox('Random Forest')
-url = 'https://github.com/suphyusinhtet/job_placement_analysis/edit/main/job_placement.csv'
+url = 'https://raw.githubusercontent.com/suphyusinhtet/job_placement_analysis/main/job_placement.csv'
 data = pd.read_csv(url)
 
 if rfmodel:

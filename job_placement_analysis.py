a4df57d910c5bcdd3953ab50c614c64c61a1d33b
suphyusinhtet <53178539+suphyusinhtet@users.noreply.github.com>
2024-03-23 11:37:55 +0630

    Update job_placement_analysis.py

--- job_placement_analysis.py
+++ job_placement_analysis.py
@@ -9,6 +9,17 @@ import seaborn as sns
 import plotly.express as px
 
 from sklearn.preprocessing import LabelEncoder
+# from imblearn.over_sampling import SMOTE
+# from sklearn.model_selection import train_test_split
+
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.naive_bayes import GaussianNB
+# from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+# from sklearn.ensemble import AdaBoostClassifier, AdaBoostRegressor
+# from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor
+# from sklearn.ensemble import ExtraTreesClassifier, ExtraTreesRegressor
+# from sklearn.linear_model import LinearRegression, LogisticRegression
+# from xgboost import XGBClassifier, XGBRegressor
 
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, mean_squared_error, r2_score, mean_absolute_error
 
@@ -20,7 +31,9 @@ print ("Dataset Length: ", len(df))
 print ("Dataset Shape: ", df.shape)
 df
 
-df.drop("id", axis=1, inplace=True)
+# df.drop("id", axis=1, inplace=True)
+df["salary"].fillna(0, inplace=True)
+df["years_of_experience"].fillna(0, inplace=True)
 print ("Dataset Shape: ", df.shape)
 df["years_of_experience"].fillna(df["years_of_experience"].median(), inplace=True)
 df["college_name"] = df["college_name"].apply(lambda x: x.split("--")[0])
@@ -33,7 +46,8 @@ df["college_name"] = df["college_name"].apply(lambda x: x.split("--")[0])
 
 """#Total values counts of Gender, Degree, Stream, College, Placement Status"""
 st.subheader('Total values counts of Gender, Degree, Stream, College, Placement Status')
-titles = ["gender", "degree", "stream", "college_name","placement_status"]
+# titles = ["gender", "degree", "stream", "college_name","placement_status"]
+titles = ["gender", "degree", "stream", "years_of_experience","college_name","placement_status"]
 
 # Set up the layout
 col1, col2 = st.columns(2)
@@ -41,51 +55,51 @@ col1, col2 = st.columns(2)
 # Iterate through titles and display value_counts
 for i, title in enumerate(titles):
     counts = df[title].value_counts()
-    if i < 3:
+    if i < 4:
         col1.header(title)
         col1.write(counts)
     else:
         col2.header(title)
         col2.write(counts)
 
-#Filter colleges that have only one person
-college_counts = df['college_name'].value_counts()
-valid_colleges = college_counts[college_counts > 1].index
-filtered_df = df[df['college_name'].isin(valid_colleges)]
+# #Filter colleges that have only one person
+# college_counts = df['college_name'].value_counts()
+# valid_colleges = college_counts[college_counts > 1].index
+# filtered_df = df[df['college_name'].isin(valid_colleges)]
 
 """#Visualize gender of each Placement Status"""
 st.subheader('Gender for Placement Status')
-figure = px.parallel_categories(filtered_df[[titles[0], titles[-1]]])
+figure = px.parallel_categories(df[[titles[0], titles[-1]]])
 st.write(figure)
-crosstab_result = pd.crosstab(filtered_df[titles[0]], filtered_df[titles[-1]])
+crosstab_result = pd.crosstab(df[titles[0]], df[titles[-1]])
 st.write(crosstab_result)
 
 """#Visualize stream of each Placement Status"""
 st.subheader('Stream for Placement Status')
-# fig = px.parallel_categories(filtered_df[[titles[2], titles[-1]]])
+# fig = px.parallel_categories(df[[titles[2], titles[-1]]])
 # st.write(fig)
-# crosstab_result_1 = pd.crosstab(filtered_df[titles[2]], filtered_df[titles[-1]])
+# crosstab_result_1 = pd.crosstab(df[titles[2]], df[titles[-1]])
 # st.write(crosstab_result_1)
-res = filtered_df.groupby(['stream','placement_status'])['placement_status'].size().reset_index(name='placement_size')
-agegp = px.bar(data_frame=res, x='stream', y='placement_size', color='placement_status', title='Placement Status by College').update_layout(xaxis_title='stream', yaxis_title='placement size')
+res = df.groupby(['stream','placement_status'])['placement_status'].size().reset_index(name='placement_size')
+agegp = px.bar(data_frame=res, x='stream', y='placement_size', color='placement_status', title='Placement Status by Stream').update_layout(xaxis_title='stream', yaxis_title='placement size')
 st.plotly_chart(agegp)
 
 """#Visualize college of each Placement Status"""
 st.subheader('College for Placement Status')
-res1 = filtered_df.groupby(['college_name','placement_status'])['placement_status'].size().reset_index(name='placement_size')
+res1 = df.groupby(['college_name','placement_status'])['placement_status'].size().reset_index(name='placement_size')
 # st.write(res1)
 agegp = px.bar(data_frame=res1, x='college_name', y='placement_size', color='placement_status', title='Placement Status by College').update_layout(xaxis_title='college', yaxis_title='placement size')
 st.plotly_chart(agegp)
 
-res2 = filtered_df.groupby(['age','placement_status'])['placement_status'].size().reset_index(name='placement_size')
+res2 = df.groupby(['age','placement_status'])['placement_status'].size().reset_index(name='placement_size')
 agegp = px.bar(data_frame=res2, x='age', y='placement_size', color='placement_status', title='Placement Status by Age').update_layout(xaxis_title='age', yaxis_title='placement size')
 st.plotly_chart(agegp)
 
-res3 = filtered_df.groupby(['years_of_experience','placement_status'])['placement_status'].size().reset_index(name='placement_size')
+res3 = df.groupby(['years_of_experience','placement_status'])['placement_status'].size().reset_index(name='placement_size')
 agegp = px.bar(data_frame=res3, x='years_of_experience', y='placement_size', color='placement_status', title='Placement Status by Years of experience').update_layout(xaxis_title='years of experience', yaxis_title='placement size')
 st.plotly_chart(agegp)
 
-res4 = filtered_df.groupby(['gpa','placement_status'])['placement_status'].size().reset_index(name='placement_size')
+res4 = df.groupby(['gpa','placement_status'])['placement_status'].size().reset_index(name='placement_size')
 agegp = px.bar(data_frame=res4, x='gpa', y='placement_size', color='placement_status', title='Placement Status by GPA').update_layout(xaxis_title='GPA', yaxis_title='placement size')
 st.plotly_chart(agegp)
 
@@ -111,7 +125,7 @@ def plots(df, name, nums, axes):
     axes[2].set_ylabel(nums)
 
 # Filter the dataframe for placed students
-temp_df = filtered_df[filtered_df["placement_status"] == "Placed"].copy()
+temp_df = df[df["placement_status"] == "Placed"].copy()
 
 # Set up the layout
 fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(14, 7))
@@ -124,12 +138,13 @@ st.pyplot(fig)
 
 le = LabelEncoder()
 
-for i in ["gender", "stream", "placement_status"]:
-    filtered_df[i] = le.fit_transform(filtered_df[i])
+for i in ["gender", "placement_status"]:
+    df[i] = le.fit_transform(df[i])
 
+st.write(df)
 st.subheader('Correlation Matrix')
 #Create a correlation matrix to show relationship between select variables
-corr_matrix = filtered_df[["gender","age","stream","gpa","years_of_experience","salary","placement_status"]].corr()
+corr_matrix = df[["gender","age","gpa","salary","years_of_experience","placement_status"]].corr()
 corr_matrix
 
 st.subheader('Correlation between Selected Variables')
@@ -137,5 +152,3 @@ fig, ax = plt.subplots(figsize=[8, 8])
 sns.heatmap(corr_matrix, annot=True, cmap='Reds', ax=ax)
 # ax.set_title("Correlation between Selected Variables")
 st.pyplot(fig)
-
-features = ["gender", "age", "stream", "gpa", "years_of_experience"]
